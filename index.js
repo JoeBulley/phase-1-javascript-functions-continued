3cf09bb53c1b7162b37e6238c8aea324d39dd7b6
JoeBulley <bulleyyy@gmail.com>
2021-11-08 11:52:16 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+function saturdayFun(event = `roller-skate`) {
+    //event = (typeof event !== 'undefined') ?  event : "roller-skate"
+    return `This Saturday, I want to ${event}!`
+}
+
+const mondayWork = function(event = `go to the office`) {
+    //event = (typeof event !== 'undefined') ? event : "go to the office"
+    return `This Monday, I will ${event}.`
+}
+
+function wrapAdjective(flair = `*`) {
+    return function applyAdjective(adjective = `special`) {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+
+}
